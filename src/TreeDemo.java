0965e1addc4d9284b98e6d0610ae38e48462c940
unsubble <unsubble@gmail.com>
2023-11-07 20:02:24 +0300

    depth and search methods added.

--- src/TreeDemo.java
+++ src/TreeDemo.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -41,6 +40,63 @@ public class TreeDemo <T extends Comparable<T>> implements Iterable<T>{
 		} else {
 			throw new IllegalArgumentException();
 		}
+	}
+	
+	/*public void remove(T val) {
+		if (search(val)) {
+			for (int c : )
+		}			
+	}
+	*/
+	
+	public int getDepth() {
+		return root == null ? 0 : (getDepth(root) + 1);
+	}
+	
+	private int getDepth(Node node) {
+		int l = 0;
+		int r = 0;
+		if (node.left != null) 
+			l = getDepth(node.left) + 1;
+		if (node.right != null)
+			r = getDepth(node.right) + 1;
+		return Math.max(l, r);
+	}
+	
+	public int getDepthOfValue(T val) {
+		return search(val) ? getDepthOfValue(root, val, 1) : -1;
+	}
+	
+	private int getDepthOfValue(Node node, T val, int d) {
+		if (node.compareTo(val) > 0)
+			return getDepthOfValue(node.left, val, d + 1);
+		else if (node.compareTo(val) < 0)
+			return getDepthOfValue(node.right, val, d + 1);
+		return d;
+	}
+	
+	public boolean search(T val) {
+		return root == null ? false : searchLeft(root, val);
+	}
+	
+	private boolean searchRight(Node node, T val) {
+		if (node == null) 
+			return false;
+		if (node.val.compareTo(val) > 0)
+			return searchLeft(node.left, val);
+		if (node.val.compareTo(val) < 0)
+			return searchRight(node.right, val);
+		return true;
+	}
+	
+	private boolean searchLeft(Node node, T val) {
+		if (node == null) 
+			return false;
+		if (node.val.compareTo(val) > 0)
+			return searchLeft(node.left, val);
+		if (node.val.compareTo(val) < 0)
+			return searchRight(node.right, val);
+		return true;
 	}
 	
 	@Override
